6f9e15c5dd55fb10a48274ddab7f3908bca66dc0
Haysansan <haysan.eang@student.cadt.edu.kh>
2026-02-15 13:03:51 +0700

    service correction

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -8,26 +8,34 @@ class RidesService {
   //
   //  filter the rides starting from given departure location
   //
-  static List<Ride> _filterByDeparture(Location departure) {
-    return availableRides
-        .where((ride) => ride.departureLocation == departure)
-        .toList();
+   static List<Ride> _filterByDeparture(List<Ride> rides, Location departure) {
+    return rides.where((ride) => ride.departureLocation == departure).toList();
   }
 
   //
   //  filter the rides starting for the given requested seat number
   //
-  static List<Ride> _filterBySeatRequested(int requestedSeat) {
-
-    return availableRides
-        .where((ride) => ride.availableSeats >= requestedSeat)
-        .toList();
+  static List<Ride> _filterBySeatRequested(
+    List<Ride> rides,
+    int requestedSeat,
+  ) {
+    return rides.where((ride) => ride.availableSeats >= requestedSeat).toList();
   }
 
   //
   //  filter the rides   with several optional criteria (flexible filter options)
   //
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    return [];
+    List<Ride> results = availableRides;
+    
+    if (departure != null) {
+      results = _filterByDeparture(results, departure);
+    }
+    
+    if (seatRequested != null) {
+      results = _filterBySeatRequested(results, seatRequested);
+    }
+    
+    return results;
   }
 }
